bdd5d967211e6bd28a447f19414f157c5df62393
Developer Rome <vero.nikitina@libero.it>
2026-01-13 15:58:45 +0100

    Add shop page placeholder

---
+++ homepage-nextjs/src/app/shop/page.tsx
@@ -0,0 +1,10 @@
+export default function ShopPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-white">
+      <div className="text-center">
+        <h1 className="text-4xl font-light mb-4">Shop</h1>
+        <p className="text-lg">Pagina shop in costruzione</p>
+      </div>
+    </div>
+  );
+}
